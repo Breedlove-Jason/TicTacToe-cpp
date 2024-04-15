c54464bd79a226af320dfaf4f8286894ba5662ba
breedlove-jason <j@jasonbreedlove.com>
2024-04-15 14:17:51 -0700

    first commit, TicTacToe

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+const int BOARD_SIZE = 3;
+using BoardType = array<array<char, BOARD_SIZE>, BOARD_SIZE>;
+
+int main() {
+
+    return 0;
+}
