524c2dd9a8cdad6b7a87410056d7a1cf209c90d5
breedlove-jason <j@jasonbreedlove.com>
2024-04-15 14:47:14 -0700

    draw gameboard

--- main.cpp
+++ main.cpp
@@ -8,7 +8,35 @@ using namespace std;
 const int BOARD_SIZE = 3;
 using BoardType = array<array<char, BOARD_SIZE>, BOARD_SIZE>;
 
-int main() {
+void initializeBoard(BoardType &board);
 
+void printBoard(BoardType &board);
+
+int main() {
+    BoardType board;
+    initializeBoard(board);
+    printBoard(board);
+    
     return 0;
 }
+
+void printBoard(BoardType &board) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            cout << board[i][j];
+            if (j < BOARD_SIZE - 1) {
+                cout << "|";
+            }
+        }
+        if (i < BOARD_SIZE - 1) {
+            cout << "\n-+-+-\n";
+        }
+    }
+    cout << "\n";
+} //end printBoard
+
+void initializeBoard(BoardType &board) {
+    for (auto &row: board) {
+        row.fill(' ');
+    }
+} //end initializeBoard
