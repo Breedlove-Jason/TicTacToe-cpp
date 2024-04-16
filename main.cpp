b68be4e296e240d74e207a2f827c86809c25b056
Jason Breedlove <j@jasonbreedlove.com>
2024-04-15 20:28:10 -0700

    refactor functions and recursion

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <array>
-#include <vector>
-#include <string>
 #include <random>
 
 using namespace std;
@@ -15,17 +13,13 @@ mt19937 gen(rd());
 uniform_int_distribution<> dis(0, 2);
 
 
-
 void initializeBoard(BoardType &board);
-
-void printBoard(BoardType &board);
-
-void playerMove(BoardType &board);
-
+void printBoard(const BoardType &board);
+void gameLoop(BoardType &board);
 void computerMove(BoardType &board);
-
-bool isValidMoveX(BoardType &board, int row, int col);
-bool isValidMoveO(BoardType &board);
+bool isValidMove(const BoardType &board, int row, int col);
+void makeMove(BoardType &board, int row, int col);
+bool isBoardFull(const BoardType &board);
 
 int main() {
     // generate random number
@@ -33,9 +27,26 @@ int main() {
 
     BoardType board;
     initializeBoard(board);
-//    printBoard(board);
+    while (true) {
+        if () {
+            cout << "Player X's turn" << endl;
+            gameLoop(board);
+            printBoard(board);
+            cout << "Player O's turn" << endl;
+            computerMove(board);
+            printBoard(board);
+        } else {
+            cout << "Player O's turn" << endl;
+            computerMove(board);
+            printBoard(board);
+            cout << "Player X's turn" << endl;
+            gameLoop(board);
+            printBoard(board);
+        }
+    }
 
-    playerMove(board);
+
+    gameLoop(board);
     printBoard(board);
     computerMove(board);
     printBoard(board);
@@ -64,9 +75,9 @@ void initializeBoard(BoardType &board) {
     }
 } //end initializeBoard
 
-void playerMove(BoardType &board) {
+void gameLoop(BoardType &board) {
     int row, col;
-    while(true){
+    while (true) {
         cout << endl;
         printBoard(board);
         cout << endl;
@@ -78,42 +89,50 @@ void playerMove(BoardType &board) {
         cout << endl;
 
 
-        if (isValidMoveX(board, row, col)){
+        if (isValidMoveX(board, row, col)) {
             board[row][col] = 'X';
-            break;
-        }else{
+            continue;
+        } else {
             cout << "Invalid move. Try again." << endl;
         }
+        cout << "Computer Turn: " << endl;
+        if (isValidMoveO(board)) {
+            computerMove(board);
+            break;
+        } else {
+            cout << "Invalid move. Try again." << endl;
+        }
+
     }
 
-} //end 'X' playerMove
+} //end 'X' gameLoop
 
 void computerMove(BoardType &board) {
     int RAND_NUM = dis(gen);
+    if (isValidMoveO(board)) {
+        board[RAND_NUM][RAND_NUM] = 'O';
 
-    board[RAND_NUM][RAND_NUM] = 'O';
-
-}//end 'O' computerMove
+    }//end 'O' computerMove
+}
 
 bool isValidMoveX(BoardType &board, int row, int col) {
     if (row < 0 || col < 0 || row >= BOARD_SIZE || col >= BOARD_SIZE || board[row][col] != ' ') {
-        cout << "Invalid move. Try again." << endl;
-        playerMove(board);
+//        cout << "Invalid move. Try again." << endl;
+//        gameLoop(board);
         return false;
     } else {
-        board[row][col] = 'X';
+//        board[row][col] = 'X';
         return true;
     }
 } //end isValidMove
 
-bool isValidMoveO(BoardType &board){
+bool isValidMoveO(BoardType &board) {
     int RAND_NUM = dis(gen);
-    if (board[RAND_NUM][RAND_NUM] != ' ' )
-    {
+    if (board[RAND_NUM][RAND_NUM] != ' ') {
         computerMove(board);
         return false;
-    }else{
+    } else {
         board[RAND_NUM][RAND_NUM] = 'O';
         return true;
     }
-}
+} //end isValidMoveO
